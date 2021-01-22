356d63e151b2691b74fd38e8026f446bc628ff18
Mircea Muresan <4398569+mmuresan@users.noreply.github.com>
2021-01-22 14:12:24 -0500

    End of 14

---
+++ client/src/app/shared/models/basket.ts
@@ -0,0 +1,27 @@
+import { Guid } from 'guid-typescript';
+
+export interface IBasket {
+  id: string;
+  items: IBasketItem[];
+}
+
+export interface IBasketItem {
+  id: number;
+  productName: string;
+  price: number;
+  quantity: number;
+  pictureUrl: string;
+  brand: string;
+  type: string;
+}
+
+export class Basket implements IBasket {
+    id = Guid.create().toString();
+    items: IBasketItem[] = [];
+}
+
+export interface IBasketTotals {
+  shipping: number;
+  subtotal: number;
+  total: number;
+}
